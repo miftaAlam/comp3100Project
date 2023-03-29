3f4b5a5381291c34a9ebf2425934516517a43d1b
Mifta Alam <tilovaalam@gmail.com>
2023-03-29 12:18:39 +1100

    Cleared some concepts

--- SocketProgramming/src/Week3Server.java
+++ SocketProgramming/src/Week3Server.java
@@ -9,21 +9,24 @@ public class Week3Server {
          Socket s = ss.accept();  // when the Client sends the SYN, the serversocket accepts the client connection --> which returns a socket
                                   // to the connection sent by that particular client (as the serversocket can listen for multiple connections)
          
-         //.getInputStream() creating a stream for the socket s to receive input (stream of data)
-         //the input stream eturned by the .getInputStream() function is stored in the din object 
-
-     //    DataInputStream din = new DataInputStream(s.getInputStream());  // receiving input
-         
+         //.getOutputStream() creates a stream for the socket s to send output (stream of data)
+         //the stream returned by .getOutputStrem is stored in the dout object
          DataOutputStream dout = new DataOutputStream(s.getOutputStream());  // sending output  
-        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-
+         // BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //Do not use System.in 
+         
+         //.getInputStream() creating a stream for the socket s to receive input (stream of data)
+         //the input stream returned by the s.getInputStream() function is stored in the din object 
+         //DataInputStream din = new DataInputStream(s.getInputStream());  // receiving input
+         //but we wrap that din object with a BufferedReader 
          BufferedReader inbr = new BufferedReader(new InputStreamReader(s.getInputStream())); 
-       
-      //    System.out.println("checking");
           System.out.println(inbr.readLine());
-          dout.writeUTF("GDAY\n");
+          dout.writeUTF("GDAY\n"); //UTF does not work well, adds in silly character in the beginning 
           System.out.println(inbr.readLine());
           dout.writeUTF("BYE\n");
+
+
+          //We are not meant to use System.in to type in the console and send messages back and forth
+          //everything needs to be automated as there are specific COMMANDS that needs to be sent back and forth automatically
 
          // String str = "HELO";
          // String str2 = "GDAY";
