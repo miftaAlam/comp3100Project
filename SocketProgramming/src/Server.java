5c2d1d0590165294c090a34a0d108445a100a426
Mifta Alam <tilovaalam@gmail.com>
2023-05-25 13:47:40 +1000

    Created a Server class, the constructor takes care of taking the String array containing the server info and initialising all the attributes of the server

---
+++ SocketProgramming/src/Server.java
@@ -0,0 +1,22 @@
+public class Server {
+    String serverType;
+    int serverID;
+    String serverState;
+    int serverCore;
+    int serverMemory;
+    int serverDisk;
+    int waitingJobs;
+    int runningJobs;
+
+    public Server(String[] serverInfoArray){
+        this.serverType = serverInfoArray[0];
+        this.serverID = Integer.parseInt(serverInfoArray[1]);
+        this.serverState = serverInfoArray[2];
+        this.serverCore = Integer.parseInt(serverInfoArray[4]); //No of Cores is in the 4th index (5th position) in the whole message
+        this.serverMemory = Integer.parseInt(serverInfoArray[5]);
+        this.serverDisk = Integer.parseInt(serverInfoArray[6]);
+        this.waitingJobs = Integer.parseInt(serverInfoArray[7]);
+        this.runningJobs = Integer.parseInt(serverInfoArray[8]);   
+    }
+}
+
